b47f92c9e35f7290a8669b37e1db6e6c738cac5a
Aardvark <aardvark@AardvarknoMacBook-puro.local>
2020-05-06 13:11:31 +0900

    feat: mkcpp のREADMEと，.gitignore 追加

--- mkcpp/mkcpp.sh
+++ mkcpp/mkcpp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-for i in `seq -w 1 10` ; do
+for i in `seq -w 1 160` ; do
     mkdir -p ${i}
     touch ${i}/a.cpp
     touch ${i}/b.cpp
     touch ${i}/c.cpp
     touch ${i}/d.cpp
 done
-
